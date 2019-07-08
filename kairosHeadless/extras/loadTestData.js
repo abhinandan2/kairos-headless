627790c20d29f20cb1a8d6e406364edd0bb84074
CORPORATE\stavan <CORPORATE\stavan@lptp-40.corporate.phonon.in>
2019-07-08 11:46:27 +0530

    update

--- kairosHeadless/extras/loadTestData.js
+++ kairosHeadless/extras/loadTestData.js
@@ -7,22 +7,22 @@ function insertTestData(id, username, password, instances, context = null, scree
 }
 
 
-/*insertTestData("centralAgent#200001","a1@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: true, headless: true });
-insertTestData("centralAgent#200002","a2@gmail.com","Test@123", 1, null, true, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200001","a1@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: true, headless: true });
+insertTestData("centralAgent#200002","a2@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200003","a3@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200004","a4@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200005","a5@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200006","a6@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200007","a7@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200008","a8@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200009","a9@gmail.com","Test@123", 1, null, true, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200009","a9@gmail.com","Test@123", 1, null, false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200010","a10@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200011","a11@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200012","a12@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200013","a13@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200014","a14@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200015","a15@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200016","a16@gmail.com","Test@123",1,null,true, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200016","a16@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200017","a17@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200018","a18@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200019","a19@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
@@ -35,7 +35,7 @@ insertTestData("centralAgent#200025","a25@gmail.com","Test@123",1,null,false, { 
 insertTestData("centralAgent#200026","a26@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200027","a27@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200028","a28@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200029","a29@gmail.com","Test@123",1,null,true, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200029","a29@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200030","a30@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200031","a31@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200032","a32@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
@@ -49,25 +49,25 @@ insertTestData("centralAgent#200039","a39@gmail.com","Test@123",1,null,false, { 
 insertTestData("centralAgent#200040","a40@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200041","a41@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200042","a42@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200043","a43@gmail.com","Test@123",1,null,true, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200043","a43@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200044","a44@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200045","a45@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });*/
+insertTestData("centralAgent#200045","a45@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200046","a46@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200047","a47@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200048","a48@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200049","a49@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200050","a50@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200051","a51@gmail.com","Test@123",1,null,true, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200051","a51@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200052","a52@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200053","a53@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200054","a54@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200055","a55@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200056","a56@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200057","a57@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200058","a58@gmail.com","Test@123",1,null,true, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200058","a58@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200059","a59@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200060","a60@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-insertTestData("centralAgent#200061","a61@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
+/*insertTestData("centralAgent#200061","a61@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200062","a62@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200063","a63@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200064","a64@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
@@ -97,7 +97,7 @@ insertTestData("centralAgent#200087","a87@gmail.com","Test@123",1,null,false, { 
 insertTestData("centralAgent#200088","a88@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200089","a89@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200090","a90@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
-/*insertTestData("centralAgent#200091","a91@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
+insertTestData("centralAgent#200091","a91@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200092","a92@gmail.com","Test@123",1,null,true, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200093","a93@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
 insertTestData("centralAgent#200094","a94@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });
@@ -109,3 +109,6 @@ insertTestData("centralAgent#200099","a99@gmail.com","Test@123",1,null,false, { 
 insertTestData("centralAgent#200100","a100@gmail.com","Test@123",1,null,false, { takeEventsScreenshot: false, headless: true });*/
 
 exports.loadTestData = loadTest;
+
+
+
